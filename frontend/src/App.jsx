e559811b529b621e02c344ccc791145d438bb88f
LionelRohner <lionel.rohner@gmail.com>
2026-03-21 00:03:52 -0400

    fix svg issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import "./App.css";
-import cowSVG from "./assets/beef_001.svg"
+import cowSVG from "./assets/beef_001.svg";
 
 function App() {
   return (
     <div className="app">
       <h1>🥩MeatMap🥩</h1>
       <p>Explore meat cuts in different languages.</p>
-      <<img
+      <img
         src={cowSVG}
         alt="CowDiag"
         className="cow-diag"
         onClick={() => alert("Clicked!")}
-      </div>
+      />
     </div>
   );
 }
